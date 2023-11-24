acee5d07b1670f6765d265ed5ce7f9f141552fe0
ilhamrizk <tetcettet@gmail.com>
2023-11-25 00:35:14 +0700

    ready to be

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from main import getSimilarProduct
+from main import *
 from flask import Flask, request, jsonify
 
 import pandas as pd
@@ -7,30 +7,65 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    # random generate from PoS
-    df_pos = pd.read_excel('Product Name from PoS Transactions.xlsx')\
-        .sample()\
-        .reset_index()
-    pos = df_pos.head()['Product Name'][0]
-    print(pos)
-
-    out = getSimilarProduct(pos)
-    return out
+    return "main"
 
 @app.route('/getSimilarProduct', methods=['GET'])
-def process_input():
+def processGetSimilar():
     data = request.json  # Assuming input is in JSON format
     if data is None:
         return jsonify({'error': 'No data provided'}), 400
     
     # Perform some action based on the input
-    pos = data.get('pos')  # Replace 'input_data' with your input field
-    print(pos)
-
-    # Example action: reversing the input string
+    pos = data.get('pos')
+    print(data)
+    wr = data.get('write')
+    print(wr)
+    
+    # Action
     if pos is not None and isinstance(pos, str):
         processed_result = getSimilarProduct(pos)
+        print(processed_result)
+        try:
+            print(wr.upper())
+            inserttoData(processed_result)
+        except:
+            print('nowrite')
         return jsonify({'result': processed_result})
+    else:
+        return jsonify({'error': 'Invalid input format or field'}), 400
+
+@app.route('/getListSimilar', methods=['GET'])
+def processGetList():
+    data = request.json  # Assuming input is in JSON format
+    if data is None:
+        return jsonify({'error': 'No data provided'}), 400
+    
+    # Perform some action based on the input
+    pos = data.get('pos')
+    print("PoS: ", pos)
+
+   # Example action: reversing the input string
+    if pos is not None and isinstance(pos, str):
+        processed_result = getListSimilar(pos)
+        print(processed_result)
+        return jsonify({'result': processed_result})
+    else:
+        return jsonify({'error': 'Invalid input format or field'}), 400
+
+@app.route('/insertSimilar', methods=['POST'])
+def processInput():
+    data = request.json  # Assuming input is in JSON format
+    if data is None:
+        return jsonify({'error': 'No data provided'}), 400
+    
+    # Perform some action based on the input
+    index = data.get('index')  # Replace 'input_data' with your input field
+    print(index)
+
+    # Example action: reversing the input string
+    if index is not None and isinstance(index, int):
+        insertbyIndex(index)
+        return jsonify({'result': 'success'})
     else:
         return jsonify({'error': 'Invalid input format or field'}), 400
 
--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import numpy as np
 import pandas as pd
 
 import re # regex
+from datetime import datetime
+from operator import itemgetter
 
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+
+THRESHOLD = .2
 
 # define functions ===================================================#
 def removeSpecial(text):
@@ -23,13 +27,14 @@ def removeSpecialNumbers(text):
     return cleaned_text.strip()  # Remove leading and trailing spaces
 
 stop_words = set(stopwords.words('indonesian'))
+stop_words.update(['pupuk', 'coklat', 'butiran'])
 def tokenize_word(string):
     str_clean = removeSpecial(str(string).lower())
     tokenized = word_tokenize(str_clean)
     return [word for word in set(tokenized) if word not in stop_words]
 
 def tokenize_word_number(string):
-    stop_words.update(['pupuk', 'gram', 'lt', 'liter', 'litre', 'gr', 'kg', 'ml', 'l', 'ltr'])
+    stop_words.update(['gram', 'lt', 'liter', 'litre', 'gr', 'kg', 'ml', 'l', 'ltr'])
     str_clean = removeSpecialNumbers(str(string).lower())
     tokenized = word_tokenize(str_clean)
     return [word for word in set(tokenized) if word not in stop_words]
@@ -41,8 +46,7 @@ def jaccard_similarity(a, b):
     return intersection / union
 
 # data all pupuk =====================================================#
-df_pupuk = pd.read_pickle('data_pupuk_all_ready.pkl')\
-    .reset_index()
+df_pupuk = pd.read_pickle('data_pupuk_all_ready.pkl')
 descriptions = [scr for scr in df_pupuk.T.to_dict().values()]
 
 # Get similarity Functions ===========================================#
@@ -63,18 +67,16 @@ def getSimilarProduct(pos):
                                 'match_raw_clean': desc['description_clean'],
                                 'match_raw_nonumber': desc['description_nonumber']}
             local_match.append(local_match_temp)
-
-    if local_match.__len__() == 0:
-        match_out = {'pos_name': pos,
-                        'pos_rawtext_clean': product_name_clean,
-                        'pos_rawtext_nonumber': product_name_nonumber,
-                        'similarity_lv1': 0,
-                        'match_id': None,
-                        'match_text': None,
-                        'match_raw_clean': None,
-                        'match_raw_nonumber': None,
-                        'similarity_lv2': 0}
-    else:
+    match_out = {'pos_name': pos,
+                'pos_rawtext_clean': product_name_clean,
+                'pos_rawtext_nonumber': product_name_nonumber,
+                'similarity_lv1': 0,
+                'match_id': None,
+                'match_text': None,
+                'match_raw_clean': None,
+                'match_raw_nonumber': None,
+                'similarity_lv2': 0}
+    if local_match.__len__() != 0:
         max_similarity = 0
         for match in local_match:
             similarity_lv2 = jaccard_similarity(match['pos_rawtext_clean'], match['match_raw_clean'])
@@ -83,12 +85,100 @@ def getSimilarProduct(pos):
                 match_out['similarity_lv2'] = similarity_lv2
                 
                 max_similarity = similarity_lv2
-    
-    classification = 'match' if match_out['similarity_lv2'] >= .2 else 'not match'
-    best_match = descriptions[match_out['match_id']] if local_match.__len__() else None
+
+    classification = 'match' if match_out['similarity_lv2'] > THRESHOLD else 'not match'
+    best_match = descriptions[match_out['match_id']] if match_out['match_id'] != None else None
     output = {'time' : datetime.today(),
               'product': pos,
               'classification': classification,
               'best_match': best_match,
               'similarity': match_out['similarity_lv2']}
     return output
+
+def inserttoData(f):
+    data = pd.read_csv('data_result_similarities.csv')
+    print(f)
+    if f['best_match'] != None:
+        feedback_insert = {'id': max(data['id'])+1,
+                        'product':f['product'],
+                        'match_index': f['best_match']['index'],
+                        'match_product': f['best_match']['Product SKU'],
+                        'match_brand': f['best_match']['Brand'],
+                        'match_type': f['best_match']['Type'],
+                        'match_category': f['best_match']['Category'],
+                        'match_formula': f['best_match']['Formula'],
+                        'match_new_sku': f['best_match']['new_sku'],
+                        'match_description': f['best_match']['description'],
+                        'similarity': f['similarity'],
+                        'class': 1 if f['classification'] == 'match' else 0,
+                        'class_desc': f['classification'],
+                        'time': datetime.now()}
+    else:
+        feedback_insert = {'id': max(data['id'])+1,
+                        'product':f['product'],
+                        'match_index': None,
+                        'match_product': None,
+                        'match_brand': None,
+                        'match_type': None,
+                        'match_category': None,
+                        'match_formula': None,
+                        'match_new_sku': None,
+                        'match_description': None,
+                        'similarity': f['similarity'],
+                        'class': 1 if f['classification'] == 'match' else 0,
+                        'class_desc': f['classification'],
+                        'time': datetime.now()}
+    df_toinsert = pd.DataFrame([feedback_insert])
+    df_toinsert.to_csv('data_result_similarities.csv', mode='a', index=False, header=False)
+
+# Get similar list ==========================================#
+def getListSimilar(pos):
+    product_name_clean = tokenize_word(pos)
+    product_name_nonumber = tokenize_word_number(pos)
+
+    local_match = []
+    for desc in descriptions:
+        similarity_lv1 = jaccard_similarity(product_name_nonumber, desc['description_nonumber'])
+        if similarity_lv1 > 0:
+            local_match_temp = {'pos_name': pos,
+                                'pos_rawtext_clean': product_name_clean,
+                                'pos_rawtext_nonumber': product_name_nonumber,
+                                'similarity_lv1': similarity_lv1,
+                                'match_id': desc['index'],
+                                'match_text': desc['Product SKU'],
+                                'match_raw_clean': desc['description_clean'],
+                                'match_raw_nonumber': desc['description_nonumber']}
+            local_match.append(local_match_temp)
+    
+    match_out = []
+    for match in local_match:
+        similarity_lv2 = jaccard_similarity(match['pos_rawtext_clean'], match['match_raw_clean'])
+        if similarity_lv2 > THRESHOLD:
+            desc_out = descriptions[match['match_id']]
+            desc_out['similarity'] = similarity_lv2
+            match_out.append(desc_out)
+            del desc_out['description_nonumber']
+            del desc_out['description_clean']
+    print(len(match_out))
+    return sorted(match_out, key=itemgetter('similarity'), reverse=True)
+
+def insertbyIndex(i):
+    data = pd.read_csv('data_result_similarities.csv')
+    f = descriptions[int(i)]
+    print(f)
+    feedback_insert = {'id': max(data['id'])+1,
+                    'product': f['Product SKU'],
+                    'match_index': f['index'],
+                    'match_product': f['Product SKU'],
+                    'match_brand': f['Brand'],
+                    'match_type': f['Type'],
+                    'match_category': f['Category'],
+                    'match_formula': f['Formula'],
+                    'match_new_sku': f['new_sku'],
+                    'match_description': f['description'],
+                    'similarity': 1,
+                    'class': 1,
+                    'class_desc': 'match',
+                    'time': datetime.now()}
+    df_toinsert = pd.DataFrame([feedback_insert])
+    df_toinsert.to_csv('data_result_similarities.csv', mode='a', index=False, header=False)
